46b3466d4840a03767791e4df10272e5a6ff193e
AnderStudio <chouyuhung1@gmail.com>
2023-12-17 17:04:49 +0800

    modify process_vq_dataset.py and set up environment

--- process_vq_dataset.py
+++ process_vq_dataset.py
@@ -39,6 +39,38 @@ def process_video_annot(video_data, n_samples_so_far):
                 n_samples_so_far = n_samples_so_far + 1
     return annotations
 
+def process_video_annot_jokodatasets(video_name, video_data, n_samples_so_far):
+    
+    annotations = []
+
+    # split = video_data["split"]
+
+    for clip_annot in video_data["annotations"]:
+        for qset_id, qset in clip_annot["query_sets"].items():
+            if not qset["is_valid"]:
+                continue
+            curr_annot = {
+                # "metadata": {      
+                    # "video_start_sec": clip_data["video_start_sec"],
+                    # "video_end_sec": clip_data["video_end_sec"],
+                    # "clip_fps": clip_data["clip_fps"],
+                # },
+                # "clip_uid": clip_data["clip_uid"],
+
+                "video_uid": video_name,
+                "query_set": qset_id,
+                "query_frame": qset["query_frame"],
+                "response_track": qset["response_track"],
+                "visual_crop": qset["visual_crop"],
+                "object_title": qset["object_title"],
+
+                # Assign a unique ID to this annotation for the dataset
+                # "dataset_uid": get_dataset_uid(split, n_samples_so_far),
+            }
+            annotations.append(curr_annot)
+            n_samples_so_far = n_samples_so_far + 1
+
+    return annotations
 
 def process_data(args):
     annotation_exports = {
@@ -61,24 +93,32 @@ def process_data(args):
         n_samples = 0
         split_data = []
         print(f"========> Processing {split} data")
-        for video_data in tqdm.tqdm(split_export["videos"]):
-            proc_video_data = process_video_annot(video_data, n_samples)
+
+        # for video_data in tqdm.tqdm(split_export["videos"]):
+        #     proc_video_data = process_video_annot(video_data, n_samples)
+        #     split_data += proc_video_data
+        #     n_samples += len(proc_video_data)
+
+        for video_name in tqdm.tqdm(split_export.keys()):
+            proc_video_data = process_video_annot_jokodatasets(video_name, split_export[video_name], n_samples)
             split_data += proc_video_data
             n_samples += len(proc_video_data)
+
         save_path = os.path.join(args.save_root, f"{split}_annot.json.gz")
         with gzip.open(save_path, "wt") as fp:
             json.dump(split_data, fp)
+
         # Print dataset statistics
-        uniq_videos = set()
-        uniq_clips = set()
-        n_annots = 0
-        for data in split_data:
-            uniq_videos.add(data["metadata"]["video_uid"])
-            uniq_clips.add(data["clip_uid"])
-            n_annots += 1
-        print(f"# videos     : {len(uniq_videos)}")
-        print(f"# clips      : {len(uniq_clips)}")
-        print(f"# annotations: {n_annots}")
+        # uniq_videos = set()
+        # uniq_clips = set()
+        # n_annots = 0
+        # for data in split_data:
+        #     uniq_videos.add(data["metadata"]["video_uid"])
+        #     uniq_clips.add(data["clip_uid"])
+        #     n_annots += 1
+        # print(f"# videos     : {len(uniq_videos)}")
+        # print(f"# clips      : {len(uniq_clips)}")
+        # print(f"# annotations: {n_annots}")
 
 
 if __name__ == "__main__":
